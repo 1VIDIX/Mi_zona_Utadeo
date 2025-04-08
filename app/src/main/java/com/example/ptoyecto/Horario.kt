ce7e6922060ae008b9ea9f6c92c4376e2bc01b04
juand <juandiegopg88@gmail.com>
2025-04-07 20:26:42 -0500

    boton de horario en el menú, y solo la base de su interfaz

---
+++ app/src/main/java/com/example/ptoyecto/Horario.kt
@@ -0,0 +1,43 @@
+package com.example.ptoyecto
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class Horario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_horario)
+        val welcomeTextView: TextView = findViewById(R.id.textView3)
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        if (user != null) {
+            val uid = user.uid
+            val db = FirebaseFirestore.getInstance()
+            val docRef = db.collection("usuarios").document(uid)
+
+            welcomeTextView.text = "Horario"
+        }
+        val back : Button = findViewById(R.id.buttonMenu)
+        back.setOnClickListener {
+            val intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
